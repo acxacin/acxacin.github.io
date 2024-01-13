1e26732b8c2619c8d1864606928ffc4caa703b6f
acxacin <156527633+acxacin@users.noreply.github.com>
2024-01-13 18:57:29 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+let points = 0;
+
+let a = "Շնորհակալություն Ճիշտ պատասխան!";
+
+let b = "Սխալ պատասխանեցիք! ճիշտ պատասխանն Է առաջինը";
+
+let c = "Սխալ պատասխանեցիք! ճիշտ պատասխանն Է առաջինը";
+
+let d = "Սխալ պատասխանեցիք! ճիշտ պատասխանն Է առաջինը";
+
+harc_1.onclick = function() {
+    alert(a);
+}
+
+harc_2.onclick = function() {
+    alert(b);
+}
+
+harc_3.onclick = function() {
+    alert(c);
+}
+
+harc_4.onclick = function() {
+    alert(d);
+}
+
+let e = "Շնորհակալություն Ճիշտ պատասխան!";
+
+let f = "Սխալ պատասխանեցիք! ճիշտ պատասխանն Է երրորդը";
+
+let g = "Սխալ պատասխանեցիք! ճիշտ պատասխանն Է երրորդը";
+
+let h = "Սխալ պատասխանեցիք! ճիշտ պատասխանն Է երրորդը";
+
+harc_5.onclick = function() {
+    alert(f);
+}
+
+harc_6.onclick = function() {
+    alert(g);
+}
+
+harc_7.onclick = function() {
+    alert(e);
+}
+
+harc_8.onclick = function() {
+    alert(h);
+}
+
+let x = "Շնորհակալություն Ճիշտ պատասխան!";
+
+let y = "Սխալ պատասխանեցիք! ճիշտ պատասխանն Է չորորդը";
+
+let z = "Սխալ պատասխանեցիք! ճիշտ պատասխանն Է չորորդը";
+
+let m = "Սխալ պատասխանեցիք! ճիշտ պատասխանն Է չորորդը";
+
+harc_9.onclick = function() {
+    alert(y);
+}
+
+harc_10.onclick = function() {
+    alert(z);
+}
+
+harc_11.onclick = function() {
+    alert(m);
+}
+
+harc_12.onclick = function() {
+    alert(x);
+}
+
+
